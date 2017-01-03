853599adffcef9ff9c180ce460337c3b023febdf
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-01-03 09:00:56 +0000

    #Issue 1: Create an async version of the SecurityLogic interface
    The existing pac4j SecurityLogic interface is heavily focused on synchronous logic. Create a new version of the interface which reflects the need to perform blocking tasks off the event loop thread and handle the results when they arrive, so that the SecurityLogic call itself can be handled from the eventloop thread.

---
+++ pac4j-async-core/src/main/java/orc/pac4j/async/core/logic/AsyncSecurityLogic.java
@@ -0,0 +1,31 @@
+package orc.pac4j.async.core.logic;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.engine.SecurityGrantedAccessAdapter;
+
+import java.util.function.Consumer;
+
+/**
+ *
+ */
+public interface AsyncSecurityLogic<R, C extends WebContext> {
+
+    /**
+     * Perform the security logic, assuming that our clients may be async and off-thread, therefore we accept a consumer
+     * callback to handle the result of this computation.
+     *
+     * Note that we expect the config, http action adapter, client names, authorizers, matchers and muiltiprofile to
+     * be supplied as part of the config and not to change from call to call. This is the normal behaviour when used
+     * in both vertx-pac4j and play-pac4j. If someone wants to override that behaviour, they can add those parameters
+     * to a non-standard call. I also reserve the right to add them to the signature again later if nevessary. 
+     *
+     * @param context the web context
+     * @param securityGrantedAccessAdapter the success adapter
+     * @param resultHandler callback to accept the result of the security logic behaviour
+     * @param parameters additional parameters
+     */
+    void perform(C context, SecurityGrantedAccessAdapter<R, C> securityGrantedAccessAdapter,
+                 Consumer<R> resultHandler, Object... parameters);
+
+
+}
