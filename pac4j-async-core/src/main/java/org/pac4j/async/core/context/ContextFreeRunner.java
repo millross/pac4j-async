1e8d53a637f869f9b840ab86806bb8c2cff645a4
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-01-20 18:33:24 +0000

    Define ContextFreeRunner default implementation of ContextRunner

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/context/ContextFreeRunner.java
@@ -0,0 +1,14 @@
+package org.pac4j.async.core.context;
+
+/**
+ * Simple context runner for the situation where we don't care about moving onto a specific context. This should be used
+ * with caution as most async frameworks make certain guarantees about thread safety based on running on a particular
+ * context (meaning that the state of an actor or similar runnnng on a particular context will only be accessed by
+ * a specific thread, ensuring thread safety for thst atate. Vert.x is an example of such an async framework.
+ */
+public class ContextFreeRunner implements ContextRunner {
+    @Override
+    public void runOnContext(Runnable operation) {
+        operation.run();
+    }
+}
