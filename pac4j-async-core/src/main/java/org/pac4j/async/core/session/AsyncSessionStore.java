780065a5ff94e0ee9e3f4d05bbe61342774ffdcf
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-02-07 19:01:05 +0000

    pac4j-async-45 Define AsyncSessionStore base interface

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/session/AsyncSessionStore.java
@@ -0,0 +1,86 @@
+package org.pac4j.async.core.session;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.context.session.SessionStore;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Asynchronous version of the SessionStore interface to allow for the fact there may be blocking i/o on a session
+ * store, meaning that it should be treated asynchronously
+ */
+public interface AsyncSessionStore<C extends WebContext> {
+
+    /**
+     * Get or create the session identifier and initialize the session with it if necessary.
+     *
+     * @param context the web context
+     * @return the session identifier
+     */
+    String getOrCreateSessionId(C context);
+
+    /**
+     * Get the object from its key in store.
+     *
+     * @param context the web context
+     * @param key the key of the object
+     * @return the object in store
+     */
+    CompletableFuture<Object> get(C context, String key);
+
+    /**
+     * Save an object in the store by its key.
+     *
+     * @param context the web context
+     * @param key the key of the object
+     * @param value the value to save in store
+     */
+    CompletableFuture<Void> set(C context, String key, Object value);
+
+    /**
+     * Destroy the web session.
+     *
+     * @param context the web context
+     * @return whether the session has been destroyed
+     */
+    default CompletableFuture<Boolean> destroySession(C context) {
+        // by default, the session has not been destroyed and this can be done immediately
+        return CompletableFuture.completedFuture(false);
+    }
+
+    /**
+     * Get the native session as a trackable object.
+     *
+     * @param context the web context
+     * @return the trackable object
+     */
+    default CompletableFuture<Optional<Object>> getTrackableSession(C context) {
+        // by default, the session store does not know how to keep track the native session
+        return CompletableFuture.completedFuture(Optional.empty());
+    }
+
+    /**
+     * Build a new session store from a trackable session.
+     *
+     * @param context the web context
+     * @param trackableSession the trackable session
+     * @return the new session store
+     */
+    default Optional<SessionStore<C>> buildFromTrackableSession(C context, Object trackableSession) {
+        // by default, the session store does not know how to build a new session store
+        return Optional.empty();
+    }
+
+    /**
+     * Renew the native session by copying all data to a new one.
+     *
+     * @param context the web context
+     * @return whether the session store has renewed the session
+     */
+    default CompletableFuture<Boolean> renewSession(C context) {
+        // by default, the session store does not know how to renew the native session
+        return CompletableFuture.completedFuture(false);
+    }
+
+}
