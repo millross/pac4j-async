fb33ccc24c380039ac6c571681cf401272c20773
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-02-16 18:57:27 +0000

    pac4j-async-57 Define Authorizer as a parameterised base interface (T extends WebContextBase)

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/authorization/authorizer/AuthorizerBase.java
@@ -0,0 +1,14 @@
+package org.pac4j.core.authorization.authorizer;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.List;
+
+/**
+ * Generalised base authorizer which can represent either a sync or an async authorizer. We can auto-convert certain
+ * sync implementors of these to async versions very easily, and this type abstraction makes this very possible
+ */
+public interface AuthorizerBase<C extends WebContextBase, R, P extends CommonProfile> {
+    R isAuthorized(C context, List<P> profiles);
+}
