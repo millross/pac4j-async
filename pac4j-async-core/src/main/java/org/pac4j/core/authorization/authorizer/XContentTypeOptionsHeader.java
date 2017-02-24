c3e1ab766533c64f75e031e8199dab37332f304d
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-02-24 17:42:13 +0000

    pac4j-async-66 Create async-convertible version of XContentTypeOptionsHeader

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/authorization/authorizer/XContentTypeOptionsHeader.java
@@ -0,0 +1,21 @@
+package org.pac4j.core.authorization.authorizer;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.List;
+
+/**
+ * XContent type options header.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.1
+ */
+public class XContentTypeOptionsHeader implements Authorizer<WebContextBase<?>, CommonProfile> {
+    @Override
+    public Boolean isAuthorized(WebContextBase<?> context, List<CommonProfile> profiles) throws HttpAction {
+        context.setResponseHeader("X-Content-Type-Options", "nosniff");
+        return true;
+    }
+}
