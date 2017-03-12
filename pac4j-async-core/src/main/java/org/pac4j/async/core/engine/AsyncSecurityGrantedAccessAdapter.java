1a51778f8c04769294dd3983a996c804f3600809
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-03-12 09:29:17 +0000

    pac4j-async-58 Define AsyncSecurityGrantedAccessAdapter interface

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/engine/AsyncSecurityGrantedAccessAdapter.java
@@ -0,0 +1,20 @@
+package org.pac4j.async.core.engine;
+
+import org.pac4j.async.core.context.AsyncWebContext;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ *
+ */
+public interface AsyncSecurityGrantedAccessAdapter<R, C extends AsyncWebContext<?>> {
+    /**
+     * Adapt the current successful action as the expected result.
+     *
+     * @param context the web context
+     * @param parameters additional parameters
+     * @return a completable future which will complete with the adapted result
+     */
+    CompletableFuture<R> adapt(C context, Object... parameters) ;
+
+}
