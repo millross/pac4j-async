dbbf6d85aa563d35254865609941ac4b2f7053b3
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-03-15 18:48:28 +0000

    pac4j-async-73 Create Async version of CsrfAuthorizer

--- pac4j-async-core/src/main/java/org/pac4j/async/core/authorization/authorizer/AsyncAuthorizer.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/authorization/authorizer/AsyncAuthorizer.java
@@ -4,12 +4,12 @@ import com.aol.cyclops.invokedynamic.ExceptionSoftener;
 import org.pac4j.async.core.AsynchronousComputation;
 import org.pac4j.async.core.context.AsyncWebContext;
 import org.pac4j.core.authorization.authorizer.Authorizer;
-import org.pac4j.core.authorization.authorizer.AuthorizerBase;
 import org.pac4j.core.context.WebContextBase;
 import org.pac4j.core.profile.CommonProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -17,8 +17,7 @@ import java.util.concurrent.CompletableFuture;
  * merely a simple type definition to keep everything on site.
  *
  */
-public interface AsyncAuthorizer<U extends CommonProfile> extends AuthorizerBase <AsyncWebContext<?>,
-        CompletableFuture<Boolean>, U> {
+public interface AsyncAuthorizer<U extends CommonProfile> {
 
     Logger LOG = LoggerFactory.getLogger(AsyncAuthorizer.class);
 
@@ -29,4 +28,7 @@ public interface AsyncAuthorizer<U extends CommonProfile> extends AuthorizerBase
     static <U extends CommonProfile> AsyncAuthorizer<U> fromNonBlockingAuthorizer(final Authorizer<WebContextBase<?>, U> syncAuthorizer) {
         return (context, profiles) -> AsynchronousComputation.fromNonBlocking(ExceptionSoftener.softenSupplier(() -> syncAuthorizer.isAuthorized(context, profiles)));
     }
+
+    CompletableFuture<Boolean> isAuthorized(AsyncWebContext<?> context, List<U> profiles);
+
 }
---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/authorization/authorizer/csrf/AsyncCsrfAuthorizer.java
@@ -0,0 +1,73 @@
+package org.pac4j.async.core.authorization.authorizer.csrf;
+
+import org.pac4j.async.core.authorization.authorizer.AsyncAuthorizer;
+import org.pac4j.async.core.context.AsyncWebContext;
+import org.pac4j.core.context.ContextHelper;
+import org.pac4j.core.context.Pac4jConstants;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ *
+ */
+public class AsyncCsrfAuthorizer implements AsyncAuthorizer<CommonProfile> {
+
+    private String parameterName = Pac4jConstants.CSRF_TOKEN;
+
+    private String headerName = Pac4jConstants.CSRF_TOKEN;
+
+    private boolean onlyCheckPostRequest = true;
+
+    public AsyncCsrfAuthorizer() {
+    }
+
+    public AsyncCsrfAuthorizer(final String parameterName, final String headerName) {
+        this.parameterName = parameterName;
+        this.headerName = headerName;
+    }
+
+    public AsyncCsrfAuthorizer(final String parameterName, final String headerName, final boolean onlyCheckPostRequest) {
+        this(parameterName, headerName);
+        this.onlyCheckPostRequest = onlyCheckPostRequest;
+    }
+
+    @Override
+    public CompletableFuture<Boolean> isAuthorized(AsyncWebContext<?> context, List<CommonProfile> profiles) {
+        final boolean checkRequest = !onlyCheckPostRequest || ContextHelper.isPost(context);
+        if (checkRequest) {
+            final String parameterToken = context.getRequestParameter(parameterName);
+            final String headerToken = context.getRequestHeader(headerName);
+            return context.getSessionAttribute(Pac4jConstants.CSRF_TOKEN)
+                    .thenApply(s -> s != null && (s.equals(parameterToken) || s.equals(headerToken)));
+        } else {
+            return CompletableFuture.completedFuture(true);
+        }
+    }
+
+    public String getParameterName() {
+        return parameterName;
+    }
+
+    public void setParameterName(String parameterName) {
+        this.parameterName = parameterName;
+    }
+
+    public String getHeaderName() {
+        return headerName;
+    }
+
+    public void setHeaderName(String headerName) {
+        this.headerName = headerName;
+    }
+
+    public boolean isOnlyCheckPostRequest() {
+        return onlyCheckPostRequest;
+    }
+
+    public void setOnlyCheckPostRequest(boolean onlyCheckPostRequest) {
+        this.onlyCheckPostRequest = onlyCheckPostRequest;
+    }
+
+}
