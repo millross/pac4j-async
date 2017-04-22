4dc93ed862739e20da32a2aa267fe688d0f9ca12
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-04-22 14:25:27 +0100

    pac4j-async-103 Modify AsyncProfileManagerTest to use new AsyncWebContext mocking strategy

--- pac4j-async-core/src/test/java/org/pac4j/async/core/profile/AsyncProfileManagerTest.java
+++ pac4j-async-core/src/test/java/org/pac4j/async/core/profile/AsyncProfileManagerTest.java
@@ -4,6 +4,7 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import org.junit.Before;
 import org.junit.Test;
+import org.pac4j.async.core.MockAsyncWebContextBuilder;
 import org.pac4j.async.core.VertxAsyncTestBase;
 import org.pac4j.async.core.context.AsyncWebContext;
 import org.pac4j.core.profile.AnonymousProfile;
@@ -18,9 +19,6 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
 import static org.pac4j.core.context.Pac4jConstants.USER_PROFILES;
 
 /**
@@ -49,34 +47,31 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
     @Before
     public void setUp() {
-        webContext = mock(AsyncWebContext.class);
+        webContext = MockAsyncWebContextBuilder.from(rule.vertx(), executionContext).build();
         // Use the test's execution context for async work here - this compares with the fact that  it is
         // effectively the execution context in force when the web context is created
-        when(webContext.getExecutionContext()).thenReturn(executionContext);
         profileManager = new AsyncProfileManager(webContext);
         profiles = new LinkedHashMap<>();
     }
 
     @Test(timeout = 1000)
     public void testGetNullProfile(final TestContext testContext) {
-        programGetSessionAttributeToReturn(null);
         executeAsyncGetTest(testContext, true, assertNoProfileReturned());
     }
 
     @Test(timeout = 1000)
     public void testGetNoProfile(final TestContext testContext) {
-        programGetSessionAttributeToReturn(profiles);
-        executeAsyncGetTest(testContext, true, assertNoProfileReturned());
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+               .thenRun(() -> executeAsyncGetTest(testContext, true, assertNoProfileReturned()));
     }
 
     @Test(timeout = 1000)
     public void testGetOneProfileFromSession(final TestContext testContext) {
 
         profiles.put(CLIENT1, PROFILE1);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetTest(testContext, true,
-                assertAuthenticatedWithProfile(PROFILE1));
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() -> executeAsyncGetTest(testContext, true,
+                        assertAuthenticatedWithProfile(PROFILE1)));
 
     }
 
@@ -85,93 +80,85 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         profiles.put("first", new AnonymousProfile());
         profiles.put(CLIENT1, PROFILE1);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetTest(testContext, true,
-                assertAuthenticatedWithProfile(PROFILE1));
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+            .thenRun(() -> executeAsyncGetTest(testContext, true,
+                assertAuthenticatedWithProfile(PROFILE1)));
     }
 
     @Test(timeout = 1000)
     public void testGetOneTwoProfilesFromSession(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
         profiles.put(CLIENT2, PROFILE2);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetTest(testContext, true,
-                assertAuthenticatedWithProfile(PROFILE1));
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetTest(testContext, true,
+                assertAuthenticatedWithProfile(PROFILE1)));
     }
 
     @Test(timeout = 1000)
     public void testGetOneProfileFromRequest(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetTest(testContext, false,
-                assertNoProfileReturned());
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetTest(testContext, false,
+                assertNoProfileReturned()));
     }
 
     @Test(timeout = 1000)
     public void testGetAllNullProfile(final TestContext testContext) {
 
-        programGetSessionAttributeToReturn(null);
-
-        executeAsyncGetAllTest(testContext, true,
-                assertNotAuthenticatedAndEmptyProfileListReturned());
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, true,
+                assertNotAuthenticatedAndEmptyProfileListReturned()));
 
     }
 
     @Test(timeout = 1000)
     public void testGetAllNoProfile(final TestContext testContext) {
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetAllTest(testContext, true,
-                assertNotAuthenticatedAndEmptyProfileListReturned());
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, true,
+                assertNotAuthenticatedAndEmptyProfileListReturned()));
 
     }
 
     @Test(timeout = 1000)
     public void testGetAllOneProfileFromSession(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetAllTest(testContext, true,
-                assertProfileListReturned(PROFILE1));
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, true,
+                assertProfileListReturned(PROFILE1)));
     }
 
     @Test(timeout = 1000)
     public void testGetAllTwoProfilesFromSession(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
         profiles.put(CLIENT2, PROFILE2);
-        programGetSessionAttributeToReturn(profiles);
-
-        executeAsyncGetAllTest(testContext, true,
-                assertProfileListReturned(PROFILE1, PROFILE2));
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, true,
+                assertProfileListReturned(PROFILE1, PROFILE2)));
     }
 
     @Test(timeout = 1000)
     public void testGetAllTwoProfilesFromSessionAndRequest(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programGetRequestAttributeToReturn(profiles);
+        webContext.setRequestAttribute(USER_PROFILES, profiles);
         final LinkedHashMap<String, CommonProfile> profiles2 = new LinkedHashMap<>();
         profiles2.put(CLIENT2, PROFILE2);
-        programGetSessionAttributeToReturn(profiles2);
-        executeAsyncGetAllTest(testContext, true,
-                assertProfileListReturned(PROFILE1, PROFILE2));
+        webContext.setSessionAttribute(USER_PROFILES, profiles2)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, true,
+                assertProfileListReturned(PROFILE1, PROFILE2)));
     }
 
     @Test(timeout = 1000)
     public void testGetAllOneProfileFromRequest(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programGetSessionAttributeToReturn(profiles);
-        executeAsyncGetAllTest(testContext, false,
-                assertEmptyProfileListReturned());
+        webContext.setSessionAttribute(USER_PROFILES, profiles)
+                .thenRun(() ->executeAsyncGetAllTest(testContext, false,
+                assertEmptyProfileListReturned()));
     }
 
     @Test(timeout = 1000)
     public void testRemoveSessionFalse(final TestContext testContext) {
 
         profiles.put(CLIENT1, PROFILE1);
-        programSessionAttributesBasedOnMap();
 
         testProfileInSessionAfter(webContext.setSessionAttribute(USER_PROFILES, profiles),
                 v -> profileManager.remove(false),
@@ -182,7 +169,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
     @Test(timeout = 1000)
     public void testRemoveSessionTrue(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programSessionAttributesBasedOnMap();
 
         testProfileInSessionAfter(webContext.setSessionAttribute(USER_PROFILES, profiles),
                 v -> profileManager.remove(true),
@@ -193,7 +179,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
     @Test(timeout = 1000)
     public void testLogoutSession(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programSessionAttributesBasedOnMap();
 
         testProfileInSessionAfter(webContext.setSessionAttribute(USER_PROFILES, profiles),
                 v -> profileManager.logout(),
@@ -206,9 +191,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
     public void testRemoveRequestFalse(final TestContext testContext) {
 
         profiles.put(CLIENT1, PROFILE1);
-        programSessionAttributesBasedOnMap();
-        programSessionAttributesBasedOnMap();
-
         webContext.setRequestAttribute(USER_PROFILES, profiles);
 
         testProfileInSessionAfter(completedFuture(null),
@@ -220,8 +202,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
     @Test(timeout = 1000)
     public void testRemoveRequestTrue(final TestContext testContext) {
         profiles.put(CLIENT1, PROFILE1);
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
 
         webContext.setRequestAttribute(USER_PROFILES, profiles);
 
@@ -236,11 +216,7 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         profiles.put(CLIENT1, PROFILE1);
 
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
-
         webContext.setRequestAttribute(USER_PROFILES, profiles);
-//        profileManager.save(true, PROFILE2, false);
 
         testProfilesInSessionAfter(profileManager.save(true, PROFILE2, false),
                 v -> completedFuture(null),
@@ -254,9 +230,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         profiles.put(CLIENT1, PROFILE1);
         profiles.put(CLIENT2, PROFILE2);
-
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
 
         webContext.setRequestAttribute(USER_PROFILES, profiles);
 
@@ -272,9 +245,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         profiles.put(CLIENT1, PROFILE1);
 
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
-
         webContext.setRequestAttribute(USER_PROFILES, profiles);
 
         testProfilesInSessionAfter(profileManager.save(true, PROFILE2, true),
@@ -289,8 +259,7 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         profiles.put(CLIENT1, PROFILE1);
         profiles.put(CLIENT2, PROFILE2);
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
+
         webContext.setRequestAttribute(USER_PROFILES, profiles);
 
         testProfilesInSessionAfter(profileManager.save(true, PROFILE3, true),
@@ -306,8 +275,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
 
         final CommonProfile profile = new CommonProfile();
         profile.setClientName(CLIENT1);
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
 
         testProfilesInSessionAfter(webContext.setSessionAttribute(USER_PROFILES, profile),
                 v -> completedFuture(null),
@@ -320,7 +287,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
         final CommonProfile profile = new CommonProfile();
         profile.setClientName(CLIENT1);
 
-        programRequestAttributesBasedOnMap();
         webContext.setRequestAttribute(USER_PROFILES, profile);
 
         final Async async = testContext.async();
@@ -336,8 +302,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
     public void testIsAuthenticatedAnonymousProfile(final TestContext testContext) {
 
         profiles.put(CLIENT1, AnonymousProfile.INSTANCE);
-        programSessionAttributesBasedOnMap();
-        programRequestAttributesBasedOnMap();
 
         final Async async = testContext.async();
 
@@ -409,63 +373,6 @@ public class AsyncProfileManagerTest extends VertxAsyncTestBase{
             assertThat(b, is(true));
             async.complete();
         }));
-    }
-
-    private void programGetSessionAttributeToReturn(final Object retval) {
-        when (webContext.getSessionAttribute(USER_PROFILES)).then(invocation -> {
-            final CompletableFuture<Object> future = new CompletableFuture<>();
-            rule.vertx().setTimer(300, v -> future.complete(retval));
-            return future;
-        });
-    }
-
-    private void programGetRequestAttributeToReturn(final Object retval) {
-        when(webContext.getRequestAttribute(USER_PROFILES)).thenReturn(retval);
-    }
-
-    private void programSessionAttributesBasedOnMap() {
-        final Map<String, Object> session = new HashMap<>();
-
-        doAnswer(invocation -> {
-            Object[] args = invocation.getArguments();
-            String key = (String) args[0];
-            Object value = args[1];
-            final CompletableFuture<Void> future = new CompletableFuture<>();
-            rule.vertx().setTimer(300, v -> {
-                session.put(key, value);
-                future.complete(null);
-            });
-            return future;
-        }).when(webContext).setSessionAttribute(anyString(), anyObject());
-
-        doAnswer(invocation -> {
-            Object[] args = invocation.getArguments();
-            String key = (String) args[0];
-            final CompletableFuture<Object> future = new CompletableFuture<>();
-            rule.vertx().setTimer(300, v -> future.complete(session.get(key)));
-            return future;
-        }).when(webContext).getSessionAttribute(anyString());
-
-    }
-
-    private void programRequestAttributesBasedOnMap() {
-
-        final Map<String, Object> requestAttributes = new HashMap<>();
-
-        doAnswer(invocation -> {
-            Object[] args = invocation.getArguments();
-            String key = (String) args[0];
-            Object value = args[1];
-            requestAttributes.put(key, value);
-            return null;
-        }).when(webContext).setRequestAttribute(anyString(), anyObject());
-
-        doAnswer(invocation -> {
-            Object[] args = invocation.getArguments();
-            String key = (String) args[0];
-            return requestAttributes.get(key);
-
-        }).when(webContext).getRequestAttribute(anyString());
     }
 
     private void executeAsyncGetTest(final TestContext testContext, final boolean fromSession,
