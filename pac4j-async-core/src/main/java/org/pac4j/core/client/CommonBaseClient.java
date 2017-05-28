1e172043de4bee5ae29bffae01d7a51afa8a1da3
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-05-28 21:33:00 +0100

    pac4j-async-134 Create async-friendly base client implementation common to both sync and async

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/client/CommonBaseClient.java
@@ -0,0 +1,85 @@
+package org.pac4j.core.client;
+
+import org.pac4j.async.core.profile.definition.ProfileDefinitionAware;
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.util.CommonHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Common base client for both async and sync clients, the sync/async base clients should inherit from
+ * Type parameters are as follows:-
+ * C = credentials implementor
+ * U = profile implementor
+ * WC = web context implementor (typically WebContext or AsyncWebContext)
+ * AG = authorization generator implementor (typically AuthorizationGenerator or AsyncAuthorizationGenerator)
+ */
+public abstract class CommonBaseClient<C extends Credentials, U extends CommonProfile, WC extends WebContextBase<?>, AG>
+        extends ProfileDefinitionAware <U>{
+
+    private String name;
+    protected List<AG> authorizationGenerators = new ArrayList<>();
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        if (CommonHelper.isBlank(this.name)) {
+            return this.getClass().getSimpleName();
+        }
+        return this.name;
+    }
+
+    /**
+     * Notify of the web session renewal.
+     *
+     * @param oldSessionId the old session identifier
+     * @param context the web context
+     */
+    public void notifySessionRenewal(final String oldSessionId, final WC context) { }
+
+    public List<AG> getAuthorizationGenerators() {
+        return this.authorizationGenerators;
+    }
+
+    public void setAuthorizationGenerators(final List<AG> authorizationGenerators) {
+        CommonHelper.assertNotNull("authorizationGenerators", authorizationGenerators);
+        this.authorizationGenerators = authorizationGenerators;
+    }
+
+    public void setAuthorizationGenerators(final AG... authorizationGenerators) {
+        CommonHelper.assertNotNull("authorizationGenerators", authorizationGenerators);
+        this.authorizationGenerators = Arrays.asList(authorizationGenerators);
+    }
+
+    /**
+     * Add an authorization generator.
+     *
+     * @param authorizationGenerator an authorizations generator
+     */
+    public void setAuthorizationGenerator(final AG authorizationGenerator) {
+        addAuthorizationGenerator(authorizationGenerator);
+    }
+
+    public void addAuthorizationGenerator(final AG authorizationGenerator) {
+        CommonHelper.assertNotNull("authorizationGenerator", authorizationGenerator);
+        this.authorizationGenerators.add(authorizationGenerator);
+    }
+
+    public void addAuthorizationGenerators(final List<AG> authorizationGenerators) {
+        CommonHelper.assertNotNull("authorizationGenerators", authorizationGenerators);
+        this.authorizationGenerators.addAll(authorizationGenerators);
+    }
+
+    @Override
+    public String toString() {
+        return CommonHelper.toString(this.getClass(), "name", getName(),
+                "authorizationGenerators", authorizationGenerators);
+    }
+
+}
