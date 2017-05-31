9a15068067682d0ee0bf207243da12eb0c0e3574
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-05-31 20:27:45 +0100

    pac4j-async-139 Add async-friendly Authenticator interface definition

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/credentials/authenticator/Authenticator.java
@@ -0,0 +1,25 @@
+package org.pac4j.core.credentials.authenticator;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.exception.CredentialsException;
+import org.pac4j.core.exception.HttpAction;
+
+/**
+ * An authenticator is responsible for validating {@link Credentials} and should throw a {@link CredentialsException}
+ * if the authentication fails.
+ *
+ * @author Jerome Leleu
+ * @since 1.7.0
+ */
+public interface Authenticator<C extends Credentials, WC extends WebContextBase<?>> {
+    /**
+     * Validate the credentials. It should throw a {@link CredentialsException} in case of failure.
+     *
+     * @param credentials the given credentials
+     * @param context the web context
+     * @throws HttpAction requires a specific HTTP action if necessary
+     * @throws CredentialsException the credentials are invalid
+     */
+    void validate(C credentials, WC context) throws HttpAction, CredentialsException;
+}
