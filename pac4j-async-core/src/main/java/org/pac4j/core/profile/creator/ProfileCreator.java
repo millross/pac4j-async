8fd90b7de9dff3e89ace8f8b82e022624f70a854
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-06-01 08:38:44 +0100

    pac4j-async-142 Define async-friendly ProfileCreator interface

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/profile/creator/ProfileCreator.java
@@ -0,0 +1,26 @@
+package org.pac4j.core.profile.creator;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.core.profile.CommonProfile;
+
+/**
+ * This interface is responsible to create a {@link CommonProfile} from a {@link Credentials}.
+ * Return <code>null</code> if no profile can be found / created.
+ * 
+ * @author Jerome Leleu
+ * @since 1.7.0
+ */
+public interface ProfileCreator<C extends Credentials, U extends CommonProfile, WC extends WebContextBase<?>> {
+
+    /**
+     * Create a profile from a credentials.
+     *
+     * @param credentials the given credentials
+     * @param context the web context
+     * @return the created profile
+     * @throws HttpAction whether an additional HTTP action is required
+     */
+    U create(C credentials, WC context) throws HttpAction;
+}
