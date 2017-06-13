8ada401b6b3701d1858b3cb0a9aa457c23813d15
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-06-13 08:17:06 +0100

    pac4j-async-124 Define async-compatible MatchingChecker interface

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/matching/MatchingChecker.java
@@ -0,0 +1,26 @@
+package org.pac4j.core.matching;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.exception.HttpAction;
+
+import java.util.Map;
+
+/**
+ * The way to check requests matching - adapted to permit conversion to async version where only non-blocking i/o is used.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.1
+ */
+public interface MatchingChecker<C extends WebContextBase<?>, M> {
+
+    /**
+     * Check if the web context matches.
+     *
+     * @param context the web context, extender of WebContextBase
+     * @param matcherNames the matchers
+     * @param matchersMap the map of matchers
+     * @return whether the web context matches
+     * @throws HttpAction whether an additional HTTP action is required
+     */
+    boolean matches(C context, String matcherNames, Map<String, M> matchersMap) throws HttpAction;
+}
