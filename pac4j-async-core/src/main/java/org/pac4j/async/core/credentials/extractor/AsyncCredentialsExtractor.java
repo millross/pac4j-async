53599df391537943726c392c7d1102152c8ea2f4
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-06-15 08:08:43 +0100

    pac4j-async-149 Define converter for non-blocking sync CredentialsExtractor to async CredentialsE...

--- pac4j-async-core/src/main/java/org/pac4j/async/core/credentials/extractor/AsyncCredentialsExtractor.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/credentials/extractor/AsyncCredentialsExtractor.java
@@ -1,7 +1,11 @@
 package org.pac4j.async.core.credentials.extractor;
 
+import com.aol.cyclops.invokedynamic.ExceptionSoftener;
+import org.pac4j.async.core.AsynchronousComputation;
 import org.pac4j.async.core.context.AsyncWebContext;
+import org.pac4j.core.context.WebContextBase;
 import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.credentials.extractor.CredentialsExtractor;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -10,6 +14,13 @@ import java.util.concurrent.CompletableFuture;
  */
 public interface AsyncCredentialsExtractor<C extends Credentials> {
 
+    /**
+     * Given an existing non-blocking but synchronous CredentialsExtractor, convert it into an async non-blocking
+     * one
+     */
+    static <C extends Credentials> AsyncCredentialsExtractor<C> fromNonBlockingExtractor(final CredentialsExtractor<C, WebContextBase<?>> syncExtractor) {
+        return context -> AsynchronousComputation.fromNonBlocking(ExceptionSoftener.softenSupplier(() -> syncExtractor.extract(context)));
+    }
     /**
      * Extract the right credentials, using asynchronouse methods.
      *
