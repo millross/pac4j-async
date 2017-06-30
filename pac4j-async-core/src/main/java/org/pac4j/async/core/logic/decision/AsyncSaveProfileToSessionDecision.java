fbb714c5eaaf3ec66a71923479866e83ed1ee88f
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-06-30 08:16:08 +0100

    pac4j-async-162 Introduce SaveProfileToSessionDecision class to determine whether or not profiles...

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/logic/decision/AsyncSaveProfileToSessionDecision.java
@@ -0,0 +1,26 @@
+package org.pac4j.async.core.logic.decision;
+
+import org.pac4j.async.core.client.AsyncClient;
+import org.pac4j.async.core.client.AsyncDirectClient;
+import org.pac4j.async.core.context.AsyncWebContext;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class AsyncSaveProfileToSessionDecision<U extends CommonProfile, C extends AsyncWebContext> {
+
+    private final boolean saveToSession;
+
+    public AsyncSaveProfileToSessionDecision(boolean useSession) {
+        saveToSession = useSession;
+    }
+
+    public boolean make(final C context, final List<AsyncClient> currentClients,
+                        final AsyncDirectClient directClient, final U profile) {
+        return saveToSession;
+    }
+
+}
