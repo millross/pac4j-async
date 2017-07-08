509ebf2f34b06aa221cae06b8a7e5f8ff02a9ae9
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-07-08 08:35:10 +0100

    pac4j-async-170 Introduce Pac4jExceptionHandler interface for dealing with async exceptions

--- pac4j-async-core/src/main/java/org/pac4j/async/core/exception/handler/AsyncExceptionHandler.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/exception/handler/AsyncExceptionHandler.java
@@ -1,21 +1,23 @@
 package org.pac4j.async.core.exception.handler;
 
+import org.pac4j.async.core.context.AsyncWebContext;
 import org.pac4j.core.exception.HttpAction;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.function.Consumer;
 
 /**
- * Simple exception unwrapper for CompletionExeeptions to unwrap the underlying exception where more than one
+ * Simple exception handling for CompletionExeeptions to unwrap the underlying exception where more than one
  * CompletionStage may be involved.
  */
-public class AsyncExceptionHandler {
+public interface AsyncExceptionHandler<T> {
 
-    public static void handleException(final Throwable t, final Consumer<Throwable> unwrappedThrowableHandler) {
+    static void handleException(final Throwable t, final Consumer<Throwable> unwrappedThrowableHandler) {
         unwrappedThrowableHandler.accept(unwrapAsyncException(t));
     }
 
-    public static HttpAction extractAsyncHttpAction(final HttpAction action, final Throwable t) throws Throwable {
+    static HttpAction extractAsyncHttpAction(final HttpAction action, final Throwable t) throws Throwable {
 
         final Throwable unwrapped = unwrapAsyncException(t);
         final HttpAction result = action != null ? action : (unwrapped instanceof HttpAction ? (HttpAction) unwrapped : null);
@@ -24,9 +26,20 @@ public class AsyncExceptionHandler {
         } else throw unwrapped;
     }
 
-    private static Throwable unwrapAsyncException(final Throwable t) {
+    static Throwable unwrapAsyncException(final Throwable t) {
         return (t instanceof CompletionException) ? t.getCause() : t;
-
     }
+
+    /**
+     * Interface to be implemented to offer custom exception handling to completable futures returned by
+     * logic (either security, callback or logout logic).
+     *
+     * A default implementation will be provided which offers use of a webContext's execution context. In addition
+     * a "do nothing" implementation will be offered which permits external implementations to handle exceptions
+     * in their own code rather than via injection of a handler into the relevant logic.
+     * @param future
+     * @return
+     */
+    CompletableFuture<T> applyExceptionHandling(final CompletableFuture<T> future, final AsyncWebContext webContext);
 
 }
