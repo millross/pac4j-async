e3b4c9ca98066a0392c75e21437503c7d7e2d46f
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-07-10 08:20:39 +0100

    pac4j-async-171 Implement WebContextBasedExceptionHandler for async exceptions

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/exception/handler/DefaultAsyncExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.pac4j.async.core.exception.handler;
+
+import com.aol.cyclops.invokedynamic.ExceptionSoftener;
+import org.pac4j.async.core.context.AsyncWebContext;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ *
+ */
+public class DefaultAsyncExceptionHandler<T> implements AsyncExceptionHandler<T> {
+    @Override
+    public CompletableFuture<T> applyExceptionHandling(CompletableFuture<T> future, AsyncWebContext webContext) {
+        return future.handle((v, t) -> {
+            if (v != null) {
+                return v;
+            } else {
+                webContext.getExecutionContext().runOnContext(ExceptionSoftener.softenRunnable(() -> {
+                    throw t;
+                }));
+                return null;
+            }
+        });
+    }
+}
