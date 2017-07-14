521413a11fc7c01b1e1672c1b682c119c91dae30
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-07-14 18:36:14 +0100

    pac4j-async-179 Implement Async-friendly RedirectActionBuilder

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/redirect/RedirectActionBuilder.java
@@ -0,0 +1,22 @@
+package org.pac4j.core.redirect;
+
+import org.pac4j.core.context.WebContextBase;
+import org.pac4j.core.exception.HttpAction;
+
+/**
+ * Return a redirection to perfom.
+ *
+ * @author Jerome Leleu
+ * @since 1.9.0
+ */
+public interface RedirectActionBuilder {
+
+    /**
+     * Return a redirect action for the web context.
+     *
+     * @param context the web context
+     * @return the redirect action
+     * @throws HttpAction requires a specific HTTP action if necessary
+     */
+    RedirectAction redirect(WebContextBase context) throws HttpAction;
+}
