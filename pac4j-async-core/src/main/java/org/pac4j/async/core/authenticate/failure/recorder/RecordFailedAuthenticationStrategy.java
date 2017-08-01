cdd9a1a168a447b203ce4ff07992ab6abe8bd0c8
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-08-01 08:11:06 +0100

    pac4j-async-198 Define interface for RecordFailedAuthStrategy (to be used by clients)

---
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/authenticate/failure/recorder/RecordFailedAuthenticationStrategy.java
@@ -0,0 +1,17 @@
+package org.pac4j.async.core.authenticate.failure.recorder;
+
+import org.pac4j.async.core.client.AsyncClient;
+import org.pac4j.async.core.context.AsyncWebContext;
+import org.pac4j.core.credentials.Credentials;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Interface governing how to record a failed authentication. Direct clients do not need to record, indirect clients
+ * do need to record the fact that they have attempted and failed. Therefore by implementing a DO_NOT_RECORD and a
+ * RECORD_IN_SESSION strategy more client code can be made common.
+ */
+public interface RecordFailedAuthenticationStrategy {
+    CompletableFuture<Credentials> recordFailedAuthentication(final AsyncClient client, final Credentials credentials, final AsyncWebContext webContext);
+    CompletableFuture<Void> clearFailedAuthentication(final AsyncClient client, final AsyncWebContext webContext);
+}
