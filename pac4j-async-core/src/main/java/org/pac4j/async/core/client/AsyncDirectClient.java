4c364a7c5f42849629589563495b8445fdc41190
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-08-19 09:45:25 +0100

    pac4j-async-202 Implement getRecordFailedAuthStrategy as final on both AsyncDirect and AsyncIndir...

--- pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncDirectClient.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncDirectClient.java
@@ -1,5 +1,7 @@
 package org.pac4j.async.core.client;
 
+import org.pac4j.async.core.authenticate.failure.recorder.RecordFailedAuth;
+import org.pac4j.async.core.authenticate.failure.recorder.RecordFailedAuthenticationStrategy;
 import org.pac4j.core.credentials.Credentials;
 import org.pac4j.core.profile.CommonProfile;
 
@@ -7,4 +9,8 @@ import org.pac4j.core.profile.CommonProfile;
  * Async version of the pacr4j sync direct client
  */
 public abstract class AsyncDirectClient<C extends Credentials, U extends CommonProfile> extends AsyncBaseClient<C, U> {
+    @Override
+    protected final RecordFailedAuthenticationStrategy authFailureRecorder() {
+        return RecordFailedAuth.DO_NOT_RECORD;
+    }
 }
--- pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncIndirectClient.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncIndirectClient.java
@@ -1,5 +1,7 @@
 package org.pac4j.async.core.client;
 
+import org.pac4j.async.core.authenticate.failure.recorder.RecordFailedAuth;
+import org.pac4j.async.core.authenticate.failure.recorder.RecordFailedAuthenticationStrategy;
 import org.pac4j.async.core.context.AsyncWebContext;
 import org.pac4j.core.credentials.Credentials;
 import org.pac4j.core.exception.HttpAction;
@@ -12,5 +14,10 @@ import org.pac4j.core.redirect.RedirectAction;
 public abstract class AsyncIndirectClient<C extends Credentials, U extends CommonProfile> extends AsyncBaseClient<C, U> {
     public RedirectAction getRedirectAction(final AsyncWebContext context) throws HttpAction {
         return null;
+    }
+
+    @Override
+    protected final RecordFailedAuthenticationStrategy authFailureRecorder() {
+        return RecordFailedAuth.RECORD_IN_SESSION;
     }
 }
