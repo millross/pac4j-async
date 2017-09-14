9616a5a088df4069fb64a99aa13d99559d07e350
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-09-14 07:08:30 +0100

    pac4j-async-236 Redefine the UrlResolver interface in terms of WebContextBase

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/http/UrlResolver.java
@@ -0,0 +1,22 @@
+package org.pac4j.core.http;
+
+import org.pac4j.core.context.WebContextBase;
+
+/**
+ * Async-friendly version of the UrlResolver interface
+ * How to compute an url.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.1
+ */
+public interface UrlResolver {
+
+    /**
+     * Compute a new URL from the provided URL and the web context.
+     *
+     * @param url the provided URL
+     * @param context the web context
+     * @return the computed URL
+     */
+    String compute(String url, WebContextBase<?> context);
+}
