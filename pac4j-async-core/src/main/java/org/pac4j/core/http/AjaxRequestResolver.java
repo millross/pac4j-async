0e80d7debeee4866136e3e9cdeabc71edfb07f98
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-09-27 07:12:09 +0100

    pac4j-async-255 Define Async-friendly AjaxRequestResolver

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/http/AjaxRequestResolver.java
@@ -0,0 +1,20 @@
+package org.pac4j.core.http;
+
+import org.pac4j.core.context.WebContext;
+
+/**
+ * Compute if a HTTP request is an AJAX one.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.0
+ */
+public interface AjaxRequestResolver {
+
+        /**
+         * Whether it is an AJAX request.
+         *
+         * @param context the web context
+         * @return whether it is an AJAX request
+         */
+        boolean isAjax(WebContext<?> context);
+}
