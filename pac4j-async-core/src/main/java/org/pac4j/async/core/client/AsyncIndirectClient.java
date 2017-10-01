2fcf7fcdaec293f6a6ded96387788fcb03294125
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-10-01 15:29:14 +0100

    pac4j-async-262 Implement AsyncIndirectClient::defaultRedirectActionBuilder

--- pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncIndirectClient.java
+++ pac4j-async-core/src/main/java/org/pac4j/async/core/client/AsyncIndirectClient.java
@@ -7,11 +7,15 @@ import org.pac4j.core.credentials.Credentials;
 import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.profile.CommonProfile;
 import org.pac4j.core.redirect.RedirectAction;
+import org.pac4j.core.redirect.RedirectActionBuilder;
 
 /**
  *
  */
 public abstract class AsyncIndirectClient<C extends Credentials, U extends CommonProfile> extends AsyncBaseClient<C, U> {
+
+    private RedirectActionBuilder redirectActionBuilder;
+
     public RedirectAction getRedirectAction(final AsyncWebContext context) throws HttpAction {
         return null;
     }
@@ -19,5 +23,19 @@ public abstract class AsyncIndirectClient<C extends Credentials, U extends Commo
     @Override
     protected final RecordFailedAuthenticationStrategy authFailureRecorder() {
         return RecordFailedAuth.RECORD_IN_SESSION;
+    }
+
+    public RedirectActionBuilder getRedirectActionBuilder() {
+        return redirectActionBuilder;
+    }
+
+    public void setRedirectActionBuilder(RedirectActionBuilder redirectActionBuilder) {
+        this.redirectActionBuilder = redirectActionBuilder;
+    }
+
+    protected void defaultRedirectActionBuilder(final RedirectActionBuilder redirectActionBuilder) {
+        if (this.redirectActionBuilder == null) {
+            this.redirectActionBuilder = redirectActionBuilder;
+        }
     }
 }
