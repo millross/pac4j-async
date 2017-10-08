b8294782681133657e8b0e345c6b54d7c576eb45
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-10-08 17:10:44 +0100

    pac4j-async-258 Define Async-friendly AuthenticatorProfileCreator

---
+++ pac4j-async-core/src/main/java/org/pac4j/core/profile/creator/AuthenticatorProfileCreator.java
@@ -0,0 +1,19 @@
+package org.pac4j.core.profile.creator;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.core.profile.CommonProfile;
+
+/**
+ *
+ */
+public class AuthenticatorProfileCreator<C extends Credentials, P extends CommonProfile> implements ProfileCreator<C, P, WebContext<?>> {
+
+    public final static AuthenticatorProfileCreator INSTANCE = new AuthenticatorProfileCreator<>();
+
+    @Override
+    public P create(C credentials, WebContext<?> context) throws HttpAction {
+        return (P) credentials.getUserProfile();
+    }
+}
