0137c2ee9c776cfd6d234e7df7dbf9a6b43e6a32
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-10-13 07:35:24 +0100

    pac4j-async-248 Define AsyncOAuth20ProfileCreator

---
+++ pac4j-async-oauth/src/main/java/org/pac4j/async/oauth/profile/creator/AsyncOAuth20ProfileCreator.java
@@ -0,0 +1,46 @@
+package org.pac4j.async.oauth.profile.creator;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequestAsync;
+import com.github.scribejava.core.model.Verb;
+import org.pac4j.async.oauth.config.OAuth20Configuration;
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.oauth.config.OAuthConfiguration;
+import org.pac4j.oauth.credentials.OAuth20Credentials;
+import org.pac4j.oauth.profile.OAuth20Profile;
+
+/**
+ *
+ */
+public class AsyncOAuth20ProfileCreator<U extends OAuth20Profile> extends AsyncOAuthProfileCreator<OAuth20Credentials, U, OAuth20Configuration, OAuth2AccessToken> {
+
+    protected AsyncOAuth20ProfileCreator(OAuth20Configuration configuration) {
+        super(configuration);
+    }
+
+    @Override
+    protected OAuth2AccessToken getAccessToken(OAuth20Credentials credentials) throws HttpAction {
+        return credentials.getAccessToken();
+    }
+
+    @Override
+    protected void signRequest(OAuth2AccessToken token, OAuthRequestAsync request) {
+        this.configuration.getService().signRequest(token, request);
+        if (this.configuration.isTokenAsHeader()) {
+            request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BEARER_HEADER_PREFIX + token.getAccessToken());
+        }
+        if (Verb.POST.equals(request.getVerb())) {
+            request.addParameter(OAuthConfiguration.OAUTH_TOKEN, token.getAccessToken());
+        }
+    }
+
+    @Override
+    protected void addAccessTokenToProfile(U profile, OAuth2AccessToken accessToken) {
+        if (profile != null) {
+            final String token = accessToken.getAccessToken();
+            logger.debug("add access_token: {} to profile", token);
+            profile.setAccessToken(token);
+        }
+    }
+}
