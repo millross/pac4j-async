73024601f5015206fe0b63aff394ea3d5613b0ec
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-10-26 07:38:34 +0100

    pac4j-async-286 Implement SecurityHandlerOptions in demo module

---
+++ vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/handler/impl/SecurityHandlerOptions.java
@@ -0,0 +1,31 @@
+package org.pac4j.async.vertx.handler.impl;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * @author Jeremy Prime
+ * @since 2.0.0
+ */
+@Accessors(chain=true)
+public class SecurityHandlerOptions {
+
+    @Getter @Setter @NonNull
+    private String clients = "";
+
+    @Getter @Setter @NonNull
+    private String authorizers = "";
+
+    @Getter @Setter @NonNull
+    private String matchers = "";
+
+    @Getter @Setter
+    private boolean multiProfile = false;
+
+    @Getter
+    @Setter
+    private boolean saveProfileInSession = false;
+
+}
