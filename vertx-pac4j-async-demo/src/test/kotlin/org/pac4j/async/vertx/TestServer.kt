246d7ba3c862159619c19a18d71ab15cfea2670e
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-05 07:41:59 +0000

    pac4j-async-313 Add a spoof login endpoint to server set up for security handler testing.

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/TestServer.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/TestServer.kt
@@ -1,8 +1,10 @@
 package org.pac4j.async.vertx
 
+import io.vertx.core.Handler
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServer
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.handler.CookieHandler
 import io.vertx.ext.web.handler.SessionHandler
@@ -10,11 +12,42 @@ import io.vertx.ext.web.handler.UserSessionHandler
 import io.vertx.ext.web.sstore.LocalSessionStore
 import io.vertx.kotlin.coroutines.awaitResult
 import org.pac4j.async.vertx.auth.Pac4jAuthProvider
+import org.pac4j.async.vertx.context.VertxAsyncWebContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Class to represent the test server for testing of the vert.x async implementation
  */
 class TestServer(val vertx: Vertx) {
+
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(TestServer.javaClass)
+
+        fun getProfileManager(rc: RoutingContext,
+                              asynchronousComputationAdapter: VertxAsynchronousComputationAdapter): VertxAsyncProfileManager<VertxAsyncWebContext> {
+            val webContext = VertxAsyncWebContext(rc, asynchronousComputationAdapter)
+            return VertxAsyncProfileManager(webContext)
+
+        }
+
+        fun spoofLoginHandler(vertx: Vertx): Handler<RoutingContext> {
+            return Handler { rc: RoutingContext ->
+                AsyncSecurityHandlerTest.LOG.info("Spoof login endpoint called")
+                AsyncSecurityHandlerTest.LOG.info("Session id = " + rc.session().id())
+
+                // Set up a pac4j user and save into the session, we can interrogate later
+                val asynchronousComputationAdapter = VertxAsynchronousComputationAdapter(vertx, vertx.orCreateContext)
+                val profileManager = getProfileManager(rc, asynchronousComputationAdapter)
+                val profile = TestProfile.from(TEST_CREDENTIALS)
+                profileManager.save(true, profile, false)
+
+                AsyncSecurityHandlerTest.LOG.info("Spoof login endpoint completing")
+                rc.response().setStatusCode(204).end()
+            }
+        }
+    }
+
 
     suspend fun start() {
 
@@ -29,8 +62,10 @@ class TestServer(val vertx: Vertx) {
             route().handler(UserSessionHandler.create(authProvider))
             route().handler(BodyHandler.create())
 
+            post("/spoofLogin").handler(spoofLoginHandler(vertx))
+
             get().handler {
-                println("Hello, world")
+                LOG.info("Hello, world")
                 it.response().end("Hello, world")
             }
         }
