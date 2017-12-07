11eeaef928628ef44acd5d78a543d21d9be436e3
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-07 07:45:32 +0000

    pac4j-async-314 Add a spoofLogin client call method in the vertx async demo testing

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/AsyncSecurityHandlerTest.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/AsyncSecurityHandlerTest.kt
@@ -4,6 +4,7 @@ import io.vertx.core.Vertx
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.RunTestOnContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
+import io.vertx.ext.web.client.WebClient
 import kotlinx.coroutines.experimental.launch
 import org.junit.Rule
 import org.junit.Test
@@ -32,6 +33,8 @@ class AsyncSecurityHandlerTest {
         val vertx = rule.vertx()
         launch {
             startServer(vertx)
+            val client = TestClient(WebClient.create(vertx))
+            client.spoofLogin()
             async.complete()
         }
         async.await()
--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/TestClient.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/TestClient.kt
@@ -1,6 +1,11 @@
 package org.pac4j.async.vertx
 
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.HttpResponse
 import io.vertx.ext.web.client.WebClient
+import io.vertx.kotlin.coroutines.awaitResult
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Test client to wrap a set of http calls to our test server. We will also record any state (such as session cookies)
@@ -8,6 +13,19 @@ import io.vertx.ext.web.client.WebClient
  */
 class TestClient(val client: WebClient) {
 
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(TestClient.javaClass)
+    }
 
+    suspend fun spoofLogin() {
+
+        val response = awaitResult<HttpResponse<Buffer>> {client.post(8080, "localhost", "/spoofLogin").send(it) }
+        if (response.statusCode() != 204) {
+            // We should fail now
+            LOG.info("statusCode: " + response.statusCode())
+            throw RuntimeException("Failed to spoof login")
+        }
+        LOG.info("spoofLogin succeeded")
+    }
 
 }
