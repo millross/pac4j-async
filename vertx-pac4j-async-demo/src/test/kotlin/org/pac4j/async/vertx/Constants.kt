4efba39aa33fa2d424e111ab03b1cd37643fb29d
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-08 07:38:09 +0000

    pac4j-async-315 Add a private endpoint to the demo test server to enable testing

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/Constants.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/Constants.kt
@@ -3,6 +3,9 @@ package org.pac4j.async.vertx
 /**
  *
  */
-val GOOD_USERNAME = "jle"
-val PASSWORD = "password"
+const val GOOD_USERNAME = "jle"
+const val PASSWORD = "password"
 val TEST_CREDENTIALS = TestCredentials(GOOD_USERNAME, PASSWORD)
+const val USER_ID_KEY = "userId"
+const val EMAIL_KEY = "email"
+const val SESSION_ID_KEY = "session_id"
