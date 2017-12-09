6886e0af230c7e5abf6456aa8a3ae1584fef7d40
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-09 09:28:01 +0000

    pac4j-async-323 Add TestProfileDefinition to demo project to be used for testing vertx implementa...

---
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/profile/TestOAuth20ProfileDefinition.kt
@@ -0,0 +1,15 @@
+package org.pac4j.async.vertx.profile
+
+import org.pac4j.async.oauth.profile.definition.OAuth20ProfileDefinition
+import org.pac4j.async.vertx.TEST_CREDENTIALS
+
+/**
+ *
+ */
+class TestOAuth20ProfileDefinition : OAuth20ProfileDefinition<TestOAuth20Profile>() {
+
+    override fun extractUserProfile(body: String?): TestOAuth20Profile {
+        return TestOAuth20Profile.from(TEST_CREDENTIALS)
+    }
+
+}
