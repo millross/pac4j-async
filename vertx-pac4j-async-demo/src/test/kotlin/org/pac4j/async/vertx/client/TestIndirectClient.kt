37b9f90d0830a8d5a847893459e5ceb9f1c51063
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-15 07:33:46 +0000

    pac4j-async-322 Add TestIndirectClient to enable testing of handlers

---
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/client/TestIndirectClient.kt
@@ -0,0 +1,31 @@
+package org.pac4j.async.vertx.client
+
+import org.pac4j.async.core.context.AsyncWebContext
+import org.pac4j.async.oauth.client.AsyncOAuth20Client
+import org.pac4j.async.oauth.config.OAuth20Configuration
+import org.pac4j.async.vertx.profile.TestOAuth20Profile
+import org.pac4j.async.vertx.profile.TestOAuth20ProfileDefinition
+import org.pac4j.async.vertx.profile.TestUrlProfileCalculator
+
+/**
+ *
+ */
+class TestIndirectClient: AsyncOAuth20Client<TestOAuth20Profile, OAuth20Configuration<TestOAuth20Profile, TestOAuth20ProfileDefinition>, TestUrlProfileCalculator>() {
+
+    var baseAuthorizationUrl: String = ""
+
+    init {
+        setConfiguration(OAuth20Configuration())
+    }
+
+    override fun notifySessionRenewal(oldSessionId: String?, context: AsyncWebContext) {
+        // Intentional noop
+    }
+
+    override fun clientInit(context: AsyncWebContext?) {
+        configuration.api = TestOAuth20Api(baseAuthorizationUrl)
+        configuration.setProfileDefinition(TestOAuth20ProfileDefinition())
+        configuration.isWithState = true
+        super.clientInit(context)
+    }
+}
