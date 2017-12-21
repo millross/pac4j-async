49119191fc46540714c54d8f9b57f7528da02b79
Jeremy Prime <jeremy.prime@millross-consultants.com>
2017-12-21 08:11:44 +0000

    pac4j-async-345 Async demo test: IndirectClient should set key and secret on the oauthconfigurati...

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/client/TestIndirectClient.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/client/TestIndirectClient.kt
@@ -6,16 +6,19 @@ import org.pac4j.async.oauth.config.OAuth20Configuration
 import org.pac4j.async.vertx.profile.TestOAuth20Profile
 import org.pac4j.async.vertx.profile.TestOAuth20ProfileDefinition
 import org.pac4j.async.vertx.profile.TestUrlProfileCalculator
+import java.util.*
 
 /**
  *
  */
 class TestIndirectClient: AsyncOAuth20Client<TestOAuth20Profile, OAuth20Configuration<TestOAuth20Profile, TestOAuth20ProfileDefinition>, TestUrlProfileCalculator>() {
 
-    var baseAuthorizationUrl: String = ""
+    var baseAuthorizationUrl: String = "http://localhost:9292"
 
     init {
         setConfiguration(OAuth20Configuration())
+        configuration.key = UUID.randomUUID().toString()
+        configuration.secret = UUID.randomUUID().toString()
     }
 
     override fun notifySessionRenewal(oldSessionId: String?, context: AsyncWebContext) {
