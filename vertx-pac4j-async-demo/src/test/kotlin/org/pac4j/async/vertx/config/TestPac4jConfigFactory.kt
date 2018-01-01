ba524b6bccf4cd76d6cf8007f7a641c7f5810c85
Jeremy Prime <jeremy.prime@millross-consultants.com>
2018-01-01 10:38:53 +0000

    pac4j-async-333 Vertx demo (test): Add factory method for direct-client-based config to enable te...

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/config/TestPac4jConfigFactory.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/config/TestPac4jConfigFactory.kt
@@ -1,11 +1,13 @@
 package org.pac4j.async.vertx.config
 
 import org.pac4j.async.core.authorization.generator.AsyncAuthorizationGenerator
+import org.pac4j.async.core.client.AsyncDirectClient
 import org.pac4j.async.core.client.AsyncIndirectClient
 import org.pac4j.async.core.config.AsyncConfig
 import org.pac4j.async.core.context.AsyncWebContext
 import org.pac4j.async.vertx.CALLBACK_URL
 import org.pac4j.async.vertx.TEST_CLIENT_NAME
+import org.pac4j.async.vertx.client.TestDirectClient
 import org.pac4j.async.vertx.client.TestIndirectClient
 import org.pac4j.core.client.Clients
 import org.pac4j.core.credentials.Credentials
@@ -21,6 +23,15 @@ class TestPac4jConfigFactory {
         val client = TestIndirectClient()
         client.setName(TEST_CLIENT_NAME)
         val clients = Clients<AsyncIndirectClient<out Credentials, out CommonProfile>, AsyncAuthorizationGenerator<CommonProfile>>(CALLBACK_URL, client)
+        config.setClients(clients)
+        return config
+    }
+
+    fun directClientConfig(): AsyncConfig<Void, CommonProfile, AsyncWebContext> {
+        val config = AsyncConfig<Void, CommonProfile, AsyncWebContext>()
+        val client = TestDirectClient("ABC")
+        client.setName(TEST_CLIENT_NAME)
+        val clients = Clients<AsyncDirectClient<out Credentials, out CommonProfile>, AsyncAuthorizationGenerator<CommonProfile>>(CALLBACK_URL, client)
         config.setClients(clients)
         return config
     }
