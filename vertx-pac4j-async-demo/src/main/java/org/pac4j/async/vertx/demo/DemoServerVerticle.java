4569f56f7d35d1b5316fb8defd437f3ef35d03c1
Jeremy Prime <jeremy.prime@millross-consultants.com>
2018-01-15 07:44:18 +0000

    pac4j-async-339 Add Facebook authenticated security to the private endpoint in the demo server

--- vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/demo/DemoServerVerticle.java
+++ vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/demo/DemoServerVerticle.java
@@ -5,10 +5,28 @@ import io.vertx.core.Context;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
+import io.vertx.ext.web.handler.UserSessionHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.sstore.SessionStore;
+import org.pac4j.async.core.authorization.generator.AsyncAuthorizationGenerator;
+import org.pac4j.async.core.client.AsyncClient;
+import org.pac4j.async.core.config.AsyncConfig;
+import org.pac4j.async.oauth.client.AsyncFacebookClient;
 import org.pac4j.async.vertx.VertxAsynchronousComputationAdapter;
+import org.pac4j.async.vertx.auth.Pac4jAuthProvider;
+import org.pac4j.async.vertx.context.VertxAsyncWebContext;
 import org.pac4j.async.vertx.demo.handler.IndexHandler;
+import org.pac4j.async.vertx.demo.handler.PrivateEndpointHandler;
 import org.pac4j.async.vertx.demo.handler.SetContentTypeHandler;
+import org.pac4j.async.vertx.handler.impl.SecurityHandlerOptions;
+import org.pac4j.async.vertx.handler.impl.VertxAsyncSecurityHandler;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.profile.CommonProfile;
 
 /**
  *
@@ -24,8 +42,30 @@ public class DemoServerVerticle extends AbstractVerticle {
         final Context context = vertx.getOrCreateContext();
         final VertxAsynchronousComputationAdapter computationAdapter = new VertxAsynchronousComputationAdapter(vertx, context);
 
+        SessionStore sessionStore = LocalSessionStore.create(vertx);
+        AuthProvider authProvider = new Pac4jAuthProvider();
+
+        router.route().handler(CookieHandler.create());
+        router.route().handler(SessionHandler.create(sessionStore));
+        router.route().handler(UserSessionHandler.create(authProvider));
+        router.route().handler(BodyHandler.create());
+
+
         router.get("/").handler(new SetContentTypeHandler(HttpHeaders.TEXT_HTML));
         router.get("/").handler(new IndexHandler(computationAdapter));
+
+        final String callbackUrl = config().getString("baseUrl") + "/callback";
+        SecurityHandlerOptions options = new SecurityHandlerOptions().setClients("FacebookClient");
+        AsyncFacebookClient asyncFacebookClient = new AsyncFacebookClient(config().getString("fbId"), config().getString("fbSecret"));
+        asyncFacebookClient.setName("FacebookClient");
+        final Clients<AsyncClient, AsyncAuthorizationGenerator<CommonProfile>> clients = new Clients<>(callbackUrl, asyncFacebookClient);;
+        final AsyncConfig<Void, CommonProfile, VertxAsyncWebContext> config = new AsyncConfig<>();
+        config.setClients(clients);
+
+        final Pac4jAuthProvider pac4jAuthProvider = new Pac4jAuthProvider();
+        router.get("/facebook/index.html").handler(new VertxAsyncSecurityHandler(vertx, context, config,
+                pac4jAuthProvider, options));
+        router.get("/facebook/index.html").handler(new PrivateEndpointHandler(computationAdapter, (rc, buf) -> rc.response().end(buf)));
 
         vertx.createHttpServer()
                 .requestHandler(router::accept)
--- vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/demo/handler/PrivateEndpointHandler.java
+++ vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/demo/handler/PrivateEndpointHandler.java
@@ -4,8 +4,8 @@ import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.templ.HandlebarsTemplateEngine;
+import lombok.RequiredArgsConstructor;
 import org.pac4j.async.core.profile.AsyncProfileManager;
-import org.pac4j.async.core.session.AsyncSessionStore;
 import org.pac4j.async.vertx.VertxAsyncProfileManager;
 import org.pac4j.async.vertx.VertxAsynchronousComputationAdapter;
 import org.pac4j.async.vertx.context.VertxAsyncWebContext;
@@ -18,34 +18,28 @@ import java.util.function.BiConsumer;
 /**
  *
  */
+@RequiredArgsConstructor
 public class PrivateEndpointHandler implements Handler<RoutingContext> {
+
+    private final VertxAsynchronousComputationAdapter asynchronousComputationAdapter;
+    private final BiConsumer<RoutingContext, Buffer> generatedContentConsumer;
+
+    private final HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create();
 
     @Override
     public void handle(RoutingContext rc) {
+        final CompletableFuture<List<CommonProfile>> profileFuture = getUserProfiles(rc, asynchronousComputationAdapter);
+        profileFuture.thenAccept(profile -> {
+            rc.put("userProfiles", profile);
 
-    }
-
-    public static Handler<RoutingContext> generateProtectedIndex(final BiConsumer<RoutingContext, Buffer> generatedContentConsumer,
-                                                                 final VertxAsynchronousComputationAdapter asynchronousComputationAdapter) {
-
-        final HandlebarsTemplateEngine engine = HandlebarsTemplateEngine.create();
-
-        return rc -> {
-            // and now delegate to the engine to render it.
-
-            final CompletableFuture<List<CommonProfile>> profileFuture = getUserProfiles(rc, asynchronousComputationAdapter);
-            profileFuture.thenAccept(profile -> {
-                rc.put("userProfiles", profile);
-
-                engine.render(rc, "", "templates/protectedIndex.hbs", res -> {
-                    if (res.succeeded()) {
-                        generatedContentConsumer.accept(rc, res.result());
-                    } else {
-                        rc.fail(res.cause());
-                    }
-                });
+            engine.render(rc, "", "templates/protectedIndex.hbs", res -> {
+                if (res.succeeded()) {
+                    generatedContentConsumer.accept(rc, res.result());
+                } else {
+                    rc.fail(res.cause());
+                }
             });
-        };
+        });
     }
 
     private static CompletableFuture<List<CommonProfile>> getUserProfiles(final RoutingContext rc, final VertxAsynchronousComputationAdapter asynchronousComputationAdapter) {
