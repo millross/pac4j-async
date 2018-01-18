df2f0952d35bb14646063e6d58fa2ed6e5d28ea5
Jeremy Prime <jeremy.prime@millross-consultants.com>
2018-01-18 07:50:29 +0000

    pac4j-async-373 VertxAsyncCallbackHandler: set profile manager factory of logic correctly

--- vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/handler/impl/VertxAsyncCallbackHandler.java
+++ vertx-pac4j-async-demo/src/main/java/org/pac4j/async/vertx/handler/impl/VertxAsyncCallbackHandler.java
@@ -7,8 +7,8 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
 import org.pac4j.async.core.config.AsyncConfig;
-import org.pac4j.async.core.logic.AsyncCallbackLogic;
 import org.pac4j.async.core.logic.DefaultAsyncCallbackLogic;
+import org.pac4j.async.vertx.VertxAsyncProfileManager;
 import org.pac4j.async.vertx.VertxAsynchronousComputationAdapter;
 import org.pac4j.async.vertx.context.VertxAsyncWebContext;
 import org.pac4j.async.vertx.http.DefaultHttpActionAdapter;
@@ -21,7 +21,7 @@ public class VertxAsyncCallbackHandler implements Handler<RoutingContext> {
 
     protected static final Logger LOG = LoggerFactory.getLogger(VertxAsyncCallbackHandler.class);
 
-    private final AsyncCallbackLogic<Void, CommonProfile, VertxAsyncWebContext> callbackLogic;
+    private final DefaultAsyncCallbackLogic<Void, CommonProfile, VertxAsyncWebContext> callbackLogic;
     protected final VertxAsynchronousComputationAdapter asynchronousComputationAdapter;
 
     // Config elements which are all optional
@@ -39,6 +39,7 @@ public class VertxAsyncCallbackHandler implements Handler<RoutingContext> {
                 new DefaultHttpActionAdapter());
         this.defaultUrl = options.getDefaultUrl();
         this.asynchronousComputationAdapter = new VertxAsynchronousComputationAdapter(vertx, context);
+        callbackLogic.setProfileManagerFactory(VertxAsyncProfileManager::new);
 
     }
 
