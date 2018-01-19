e555096b2abff040b925bbce8b70a40e4f7173ca
Jeremy Prime <jeremy.prime@millross-consultants.com>
2018-01-19 07:17:25 +0000

    pac4j-async-375 Demo project tests: correct profile extraction by TestOAuth20ProfileDefinition

--- vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/profile/indirect/TestOAuth20ProfileDefinition.kt
+++ vertx-pac4j-async-demo/src/test/kotlin/org/pac4j/async/vertx/profile/indirect/TestOAuth20ProfileDefinition.kt
@@ -1,7 +1,9 @@
 package org.pac4j.async.vertx.profile.indirect
 
+import io.vertx.core.json.JsonObject
 import org.pac4j.async.oauth.profile.definition.OAuth20ProfileDefinition
-import org.pac4j.async.vertx.TEST_CREDENTIALS
+import org.pac4j.async.vertx.EMAIL_KEY
+import org.pac4j.async.vertx.USER_ID_KEY
 
 /**
  *
@@ -9,7 +11,12 @@ import org.pac4j.async.vertx.TEST_CREDENTIALS
 class TestOAuth20ProfileDefinition : OAuth20ProfileDefinition<TestOAuth20Profile>() {
 
     override fun extractUserProfile(body: String?): TestOAuth20Profile {
-        return TestOAuth20Profile.from(TEST_CREDENTIALS)
+        val json = JsonObject(body)
+        val userId = json.getString(USER_ID_KEY)
+        val email = json.getString(EMAIL_KEY)
+        val profile = TestOAuth20Profile(userId)
+        profile.addAttribute(EMAIL_KEY, email)
+        return profile
     }
 
 }
